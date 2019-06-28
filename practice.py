368f790641cfab216bbbd2096052f12ad0b65854
dorien909 <dill.david00@gmail.com>
2019-06-27 21:35:16 -0600

    commit 6/27/2019

---
+++ practice.py
@@ -0,0 +1,14 @@
+#variable practice
+name = "David"
+height_m = 2
+weight_kg = 90
+
+bmi = weight_kg / (height_m ** 2)
+print("bmi: ")
+print(bmi)
+if bmi < 25:
+    print(name)
+    print("is not overweight")
+else:
+    print(name)
+    print("is overweight")
